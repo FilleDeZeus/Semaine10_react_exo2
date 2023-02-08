c18237de5cedd0c2d1d92e36bf18131bb9490410
FilleDeZeus <enide26@hotmail.fr>
2023-02-08 17:13:30 +0100

    tt

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import './App.css';
 import { Inscription } from './compenents/formulaireInscription.js';
 import { Connection } from './compenents/formulaireConnection';
 export const App = ({  }) => {
-  
+// let currentFormulaire = <Inscription/> 
+let currentpage = false;
+let conditionFormulaire = currentpage === true ? <Inscription/> : <Connection/>
   return (
-    <div className="App">
-      <input type="radio" id="inscription" />
-      <label>Inscription</label>
-      <Inscription/>
-      <input type="radio" id="connection" />
-      <label>Connexion</label>
-      <Connection/>
-
+    <div>
+      {conditionFormulaire}
     </div>
   );
 }
